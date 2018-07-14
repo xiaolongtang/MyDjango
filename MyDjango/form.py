3d61d6da034b37d5d20b161b2300ae2cf0599cb2
xiaolongtang <xiaolongtang89@gmail.com>
2018-07-14 15:08:04 +0800

    add bootstrap

--- MyDjango/form.py
+++ MyDjango/form.py
@@ -2,18 +2,22 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 
-# 表单
+# return a form page
 def get_form(request):
     return render_to_response('form.html')
 
 
-# 接收请求数据
+# handle the form
 def submit(request):
     request.encoding = 'utf-8'
     if 'name' in request.GET:
-        message = 'you submit a name is ' + request.GET['name']
+        message = request.GET['name']
     else:
-        message = 'you submit an empty form'
-    return HttpResponse(message)
+        message = 'Mr Empty'
+    # return HttpResponse(message)
+    context = {}
+    context['message'] = message
+    return render(request, 'index.html', context)
